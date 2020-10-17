60f03468d62f6e27b20b4629319a0feef37e4ba8
npc203 <npc203@users.noreply.github.com>
2020-10-17 16:25:10 +0530

    added incomplete speak cog
    saving so that I don't lose progress

--- speak/__init__.py
+++ speak/__init__.py
@@ -1,5 +1,9 @@
 from .speak import Speak
+import json
+from pathlib import Path
 
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 def setup(bot):
     bot.add_cog(Speak(bot))
--- speak/speak.py
+++ speak/speak.py
@@ -1,39 +1,73 @@
 from redbot.core import commands
 import discord
-
+from random import choice
+from pathlib import Path
 class Speak(commands.Cog):
     """Set of commands to talk as others"""
     def __init__(self, bot):
         self.bot = bot
-
+        self.cache = {}
+        with open(Path(__file__).parent/"data/insult.pot",encoding="utf8") as fp:
+            self.insult = fp.read().splitlines()
+        with open(Path(__file__).parent/"data/sadme.pot",encoding="utf8") as fp:
+            self.sadme = fp.read().splitlines()
+        
     @commands.command()
-    async def tell(self,ctx,*,sentence):
+    async def tell(self,ctx,*,sentence:str):
         """Tells the given text as the yourself but with a bot tag"""
         hook = await self.get_hook(ctx)
         await ctx.message.delete()
         await hook.send(username=ctx.author.display_name,avatar_url=ctx.author.avatar_url,content=sentence)
     
     @commands.command()
-    async def tellas(self, ctx,mention:discord.Member,*,sentence):
+    async def tellas(self, ctx,mention:discord.Member,*,sentence:str):
         """Tells the given text as the mentioned users"""
         hook = await self.get_hook(ctx)
         await ctx.message.delete()
         await hook.send(username=mention.display_name,avatar_url=mention.avatar_url,content=sentence)
-    
-    async def get_hook(self,ctx):
-        for i in await ctx.channel.webhooks(): #Needs optimisation? cause iterating thro all the webhooks, everytime
-            if i.user.id == self.bot.user.id:
-                hook = i
-                break
-        else:
-            hook = await ctx.channel.create_webhook(name='bot_hook_'+str(ctx.channel)) 
+
+    @commands.group(invoke_without_command=False)
+    async def say(self,ctx):
+        """Says Stuff for the user"""
         
+
+    @say.command()
+    async def insult(self,ctx):
+        """Says lame insults, use at your own precaution"""
+        return choice(self.insult)
+        
+    async def sadme(self,ctx):
+        """Says depressing stuff about you"""
+        await ctx.invoke(choice(self.sadme))
+
+    async def get_hook(self,ctx):
+        try:
+            if ctx.channel.id not in self.cache:
+                for i in await ctx.channel.webhooks():
+                    if i.user.id == self.bot.user.id:
+                        hook = i
+                        self.cache[ctx.channel.id] = hook
+                        break
+                else:
+                    hook = await ctx.channel.create_webhook(name='red_bot_hook_'+str(ctx.channel.id)) 
+            else:
+                hook = self.cache[ctx.channel.id]
+        except discord.errors.NotFound: #Probably user deleted the hook
+            hook = await ctx.channel.create_webhook(name='red_bot_hook_'+str(ctx.channel.id))
+            
         return hook
 
-'''
+
     async def cog_command_error(self,ctx,error):
-        if isinstance(error.original,discord.errors.Forbidden):
-            halp = discord.Embed(title='Missing permissions!',description="Please give me permissions senpai",color=discord.Color.red())
-            halp.set_thumbnail(url="https://media1.tenor.com/images/9eff85aac8f21da39246ef40787864c8/tenor.gif?itemid=7357054")
-            await ctx.send(embed=halp)  
-'''
+        if hasattr(error,'original') and isinstance(error.original,discord.errors.Forbidden):
+            await ctx.send("Ensure that I have `Manage webhook` and `Manage messages` permissions from the server settings pls.") 
+        else: 
+            await self.bot.on_command_error(ctx, error, unhandled_by_cog=True)
+    
+    async def red_get_data_for_user(self, *, user_id: int):
+        # this cog does not store any data
+        return {}
+
+    async def red_delete_data_for_user(self, *, requester, user_id: int) -> None:
+        # this cog does not store any data
+        pass
