79afa91fe130f9728a29516d2cfe50485e4f3492
npc203 <npc203@users.noreply.github.com>
2020-10-29 01:55:59 +0530

    revert back to api for text generation

--- typeracer/typerace.py
+++ typeracer/typerace.py
@@ -1,9 +1,8 @@
-from redbot.core import commands, data_manager,checks
-import asyncio
+from redbot.core import commands, data_manager,checks,Config
+import asyncio,aiohttp
 from html.parser import HTMLParser
 import random,time,difflib
 from tabulate import tabulate
-from essential_generators import DocumentGenerator
 
 class HTMLFilter(HTMLParser):
     """For HTML to text properly without any dependencies.
@@ -24,20 +23,25 @@ class TypeRacer(commands.Cog):
     """A Typing Speed test cog, to give test your typing skills"""
     def __init__(self, bot):
         self.bot = bot
+        self.config = Config.get_conf(self, identifier=109171231123)
         self.filter = HTMLFilter()
         self.exclude = {'+', '|', '^', '`', '"', '$', ',', '!', '~', ':', '<', '#', '*', '-', '&', '(', '>', '%', ';', '}', "'", '_', '{', '=', ')', '?', '[', '/', '\\', ']', '.', '@'}
 
     @commands.command()
+    @commands.max_concurrency(1,commands.BucketType.user)
     async def racestart(self,ctx):
         async with ctx.typing():
-            gen = DocumentGenerator()
-            data = ''.join(ch for ch in gen.paragraph() if ch not in self.exclude)
-            data = "".join(filter(lambda x : (x.isalpha or x == " "), data))
-            data = data.split()
-            data = ' '.join(data[0:random.randint(25,45)])
+            async with aiohttp.ClientSession() as session:
+                async with session.get("http://www.randomtext.me/api/gibberish/p-1/25-45") as f:
+                    if f.status == 200:
+                        resp=await f.json()
+                    else:
+                        await ctx.send(f"Something went wrong, ERROR CODE:{f.status}")
+                        return
+        data = resp["text_out"]
         #Starting test after getting the text
         self.filter.feed(data)
-        a_string = self.filter.text
+        a_string = self.filter.text.strip()
         self.filter.text = ""
         msg = await ctx.send(f"{ctx.author.display_name} started a typing test: \n Let's Start in 3")
         for i in range(2,0,-1):
@@ -59,9 +63,10 @@ class TypeRacer(commands.Cog):
 
         #Post test calculations
         if "​" in b_string:
-            await ctx.send("Imagine cheating bruh, cm'on atleast be honest here.")
+            await ctx.send("Imagine cheating bruh, c'mon atleast be honest here.")
             return
         else:
+            debug =''
             mistakes = 0
             time_taken = end - start
             for i,s in enumerate(difflib.ndiff(a_string, b_string)):
@@ -79,16 +84,21 @@ class TypeRacer(commands.Cog):
                     (f"Characters from {author}",len(b_string)),
                     (f"Mistakes done by {author}",mistakes),
                 ]
-        note = "Every mistaken characters accounts for a mistaken word.\n example: If a word contains 2 mistaken characters then 2 words are considered wrong"
+        note = "Every mistaken characters accounts for a mistaken word.\nExample: If a word contains 2 mistaken characters then 2 words are considered wrong"
         await ctx.send(content = '```'+tabulate(verdict)+'```\nNote:\n'+note)
         
-
     @commands.command()
     async def racestop(self,ctx):
-        if 'task' in dir(self):
+        if hasattr(self,'task'):
             self.task.cancel()
         else:
             await ctx.send("You need to start the test.")
+    
+    async def on_command_error(self,ctx,error):
+        await ctx.message.delete()
+        if isinstance(error, commands.MaxConcurrencyReached):
+            await ctx.author.send('Only One Test per person')
+            return
         
     async def red_get_data_for_user(self, *, user_id: int):
         # this cog does not store any data
