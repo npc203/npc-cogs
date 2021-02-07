9e3a87e8ae907119b79d30a1d83863da3bfda76a
npc203 <npc203@users.noreply.github.com>
2021-02-07 22:33:08 +0530

    [CustomHelp] url key error hot fix

--- customhelp/customhelp.py
+++ customhelp/customhelp.py
@@ -539,6 +539,7 @@ class CustomHelp(commands.Cog):
         setting_mapping = {
             "react": "usereactions",
             "set_formatter": "iscustomhelp?",
+            "url":"This is deprecated (only here for compaitble purposes)"
             "thumbnail": "thumbnail",
         }
         val = await self.config.theme()
