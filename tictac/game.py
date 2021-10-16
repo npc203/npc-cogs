087f35f9fbbcadf05b21b3171d3d7dde007ed84c
npc203 <npc203@users.noreply.github.com>
2021-10-16 13:10:41 +0530

    [All] Cleanup and Review changes

--- tictac/game.py
+++
@@ -1,8 +0,0 @@
-class Game:
-    def __init__(self, mode: bool):
-        self.mode = mode  # True = normal mode, else reverse
-        self.board = [[0 for _ in range(3)] for _ in range(3)]
-
-    def move(self, pos, sign):
-        self.board[pos[0]][pos[1]] = sign
-        # return (is_ended,Message or None)
