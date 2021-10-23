9acb5d835b0a0f9f2f2b01402de8baf7ef207eac
AAA3A-AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2021-10-23 20:11:34 +0200

    Update base_help.py

--- customhelp/core/base_help.py
+++ customhelp/core/base_help.py
@@ -128,7 +128,8 @@ class BaguetteHelp(commands.RedHelpFormatter):
         
         delete_message = await self.config.settings.deletemessage()
         if delete_message:
-            await ctx.message.delete()
+            if ctx.channel.permissions_for(ctx.me).manage_messages:
+                await ctx.message.delete()
 
         if help_for is None or isinstance(help_for, dpy_commands.bot.BotBase):
             await self.format_bot_help(ctx, help_settings=help_settings)
