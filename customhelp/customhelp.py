0e8adc937e0c57b74fe255fa68d56c745adda975
ow0x <24418520+ow0x@users.noreply.github.com>
2021-11-12 17:53:56 +0530

    bump version

--- customhelp/customhelp.py
+++ customhelp/customhelp.py
@@ -54,7 +54,7 @@ class CustomHelp(commands.Cog):
     A custom customisable help for fun and profit
     """
 
-    __version__ = "0.8.0"
+    __version__ = "0.8.1"
 
     def __init__(self, bot: Red):
         self.bot = bot
