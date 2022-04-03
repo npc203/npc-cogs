4a98797ac1dbd3e332f53eecc8a34be67eb19080
Lemon Rose <78662983+japandotorg@users.noreply.github.com>
2022-04-04 02:21:44 +0530

    dpy2 changes

--- menubuttons/__init__.py
+++ menubuttons/__init__.py
@@ -11,5 +11,5 @@ with open(Path(__file__).parent / "info.json") as fp:
 
 async def setup(bot: Red) -> None:
     cog = MenuButtons(bot)
-    bot.add_cog(cog)
+    await bot.add_cog(cog)
     cog.create_init_task()
